c3ef01ccddfe5c08ab6c191f221a3c4d5ca5d129
James Armbrust <james.armbrust@gmail.com>
2025-02-21 10:03:35 -0700

    Added intro and monsters

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,39 @@
 'use client';
 
-import Link from 'next/link';
-import { adventurersListPath } from '@/app/lib/paths';
+// import Link from 'next/link';
+// import { adventurersListPath } from '@/app/lib/paths';
 
 const Home = () => {
   return (
     <>
       <h1 className="text-3xl font-bold">Welcome to Hire an Adventurer!</h1>
 
-      <p className="mt-4 mb-5">
-        Hire an adventurer for your next quest! You can find a list of adventurers
-        by clicking the link below.
+      <p className="mt-4 mb-5 italic">
+        Dragon destroying your town?  Minotaur mashing your village?  Goblins going berserk?!
+        Hire some adventurers to put the problem to rest!
       </p>
-      <Link href={adventurersListPath()}className="font-bold text-2xl">Adventurers</Link>
+      <p className="mt-4 mb-5">
+        In this simulation, you start with 100 gold coins.  You can hire adventurers to take
+        care of a random issue plaguing your town or village.   If you can defeat two challenges
+        before running out of gold, you win!
+      </p>
+      <p className="mt-4 mb-5">
+        This program functions similar to a simple e-commerce site.  You load you your cart
+        with adventurers to hire. You can add and remove adventurers from your cart.
+        Hiring the adventurers will deduct from your stash of gold coins.
+      </p>
+      <p className="mt-4 mb-5">
+        Once you have hired your adventurers on the cart page, you will be taken to the resolution page
+        which will display the random monster(s) that are causing the ruckus, and weather or not your
+        selected group of adventurers successfully defeated them.
+      </p>
+      <p>
+        As of this writing, the backend and database are simulated, so no progress is saved if 
+        you refresh the page. This will be rectified in a future version.
+      </p>
+      <p className="mt-4 mb-5">
+        To start, simply go to the Available Adventurers section -- the button is to the left.
+      </p>
     </>
   );
 };
--- src/app/ui/inn-logo.tsx
+++ src/app/ui/inn-logo.tsx
@@ -1,6 +1,6 @@
 const InnLogo = () => {
   return (
-    <div className={`flex flex-row items-center leading-none text-white`}>
+    <div className={ 'flex flex-row items-center leading-none text-white' }>
       <div className="text-[28px]">Hire an Adventurer!</div>
     </div>
   );
